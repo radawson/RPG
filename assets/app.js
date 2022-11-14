40fff54554a4a4e219e18ddcec28d649a151b519
Fernando Estrada <Estradafr@yahoo.com>
2022-11-13 18:24:56 -0800

    added classes, built warrior and rogue

--- assets/app.js
+++ assets/app.js
@@ -14,41 +14,92 @@ const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 const text = document.querySelector("#text");
+const text2 = document.querySelector("#text2");
 const levelText = document.querySelector("#lvlText");
 const healthText = document.querySelector("#healthText");
 const goldText = document.querySelector("#goldText");
 
 // Init Buttons
 
-button1.onclick = lookAround;
-button2.onclick = goTown;
-button3.onclick = goForrest;
+button1.onclick = warriorClass;
+button2.onclick = mageClass;
+button3.onclick = rogueClass;
 
-// * Functions
-
-function lookAround() {
-  button1.innerText = "Grab the stick";
-  button2.innerText = "Go to town";
-  // button3.innerText = "test";
-  text.innerText = 'You look around and find a stick near you. There is also a narrow path leading to town.'
-};
-
-function goTown() {
-  button1.innerText = "Go to Store";
-  button2.innerText = "Go into Forrest";
-  // button3.innerText = "test";
-  text.innerText = 'You entered the town. You see a sign that says "Store".'
-};
-
-function goForrest() {
-  button1.innerText = "Attack the bear";
-  button2.innerText = "Run away";
-  // button3.innerText = "test";
-  text.innerText = 'You hear a bear, but you can\'t see it'
-};
+// * Class Functions
 
 
-// ! Button 3 is below
-// function test() {
-//   console.log('Testing button');
-// };
+// * Warrior Class Functions 
+function warriorClass() {
+  text.innerText = 'Select your starting weapon.';
+  button1.innerText = 'Sword and Shield';
+  button2.innerText = 'Battle Axe';
+  button3.innerText = 'Dual Daggers';
+  text2.style.display = 'none';
+  button1.onclick = swordShield;
+  button2.onclick = battleAxe;
+  button3.onclick = oneHandMace;
+}
+
+function swordShield() {
+  text.innerText = 'Where would you like to explore first?'
+  button1.innerText = 'The Cave'
+  button2.innerText = 'The Old Bridge'
+  button3.innerText = 'The Twilight Forest'
+}
+
+function battleAxe() {
+  text.innerText = 'Where would you like to explore first?'
+  button1.innerText = 'The Cave'
+  button2.innerText = 'The Old Bridge'
+  button3.innerText = 'The Twilight Forest'
+}
+
+function oneHandMace() {
+  text.innerText = 'Where would you like to explore first?'
+  button1.innerText = 'The Cave'
+  button2.innerText = 'The Old Bridge'
+  button3.innerText = 'The Twilight Forest'
+}
+// ------------------------------------------------->
+
+
+// * Mage Class Functions
+function mageClass() {
+  // ! I need to build out a class for Mage still. 
+  // ! Button does not work.
+}
+
+// * Rogue Class Functions
+
+function rogueClass() {
+  text.innerText = 'Select your starting weapon.';
+  button1.innerText = 'Dual Daggers';
+  button2.innerText = 'Short Bow';
+  button3.innerText = 'Blade of Poison';
+  text2.style.display = 'none';
+  button1.onclick = dualDaggers;
+  button2.onclick = shortBow;
+  button3.onclick = poisonBlade;
+}
+
+
+function dualDaggers() {
+  text.innerText = 'Where would you like to explore first?'
+  button1.innerText = 'The Cave'
+  button2.innerText = 'The Old Bridge'
+  button3.innerText = 'The Twilight Forest'
+}
+
+function shortBow() {
+  text.innerText = 'Where would you like to explore first?'
+  button1.innerText = 'The Cave'
+  button2.innerText = 'The Old Bridge'
+  button3.innerText = 'The Twilight Forest'
+}
+
+function poisonBlade() {
+  text.innerText = 'Where would you like to explore first?'
+  button1.innerText = 'The Cave'
+  button2.innerText = 'The Old Bridge'
+  button3.innerText = 'The Twilight Forest'
+}
